885d266b2ce0d45b399eb13e39053a00f6d96442
Arun <arunkumar.vitmca@gmail.com>
2019-08-27 19:45:49 +0530

    security 001

---
+++ security_001/src/test/java/com/learnjava/springboot/security_001/Security001ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learnjava.springboot.security_001;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Security001ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

b6e45a54e68192a6d1a348bf8dddcf2c7cdf3d6a
Arun <arunkumar.vitmca@gmail.com>
2019-09-06 19:20:03 +0530

    Commit initial

---
+++ 007_Security_003_DB_Validation/src/main/java/com/learnjava/model/Role.java
@@ -0,0 +1,47 @@
+package com.learnjava.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "ROLE")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ROLE_ID")
+    private int id;
+    @Column(name = "ROLE")
+    private String role;
+    
+    
+    
+	public Role(int id, String role) {
+		super();
+		this.id = id;
+		this.role = role;
+	}
+	public Role() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+    
+    
+
+}

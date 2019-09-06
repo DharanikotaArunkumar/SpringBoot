240c2d0b20a8acf5e29afa0e8a5c59366a82a299
Arun <arunkumar.vitmca@gmail.com>
2019-09-06 13:46:33 +0530

    added Commit

---
+++ 004_Logging_002_log4j/src/main/java/com/learnjava/springboot/logging/_Logging_002_log4j/Application.java
@@ -0,0 +1,13 @@
+package com.learnjava.springboot.logging._Logging_002_log4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
